c9db625f6ccc5985b24d80dd9ccb7df9cadc7265
jahed121421 <nabilahmed121421@gmail.com>
2023-03-04 21:23:46 +0600

    fix error

--- js/java.js
+++ js/java.js
@@ -111,7 +111,7 @@ const dispalyDatas = (data) => {
                         </div>
                         <div class="w-1/2">
                         <h1 class="font-bold">Integrations</h1>
-                            <ul class="list-decimal list-inside text-left pl-4">
+                            <ul class="list-decimal list-inside text-left pl-5">
                             ${data.integrations?.length ? data.integrations.map((name) =>`<li>${name}</li>`).join(''):'No Data Found'}
                             </ul>
                         </div>
